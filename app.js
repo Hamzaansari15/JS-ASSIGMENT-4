8c86664441800e67c17cf70bfb009701038c0d4f
Hamzaansari <hamzaansaree15@gmail.com>
2022-05-29 05:49:46 -0700

    HTML-file-added

---
+++ app.js
@@ -0,0 +1,127 @@
+// var a = prompt("Enter your city name");
+// if (a === "karachi"){
+//     alert ("Welcome to the city of light");
+// }else {
+//     alert("You are not from karachi");
+// // }
+// var gender = prompt("enter your gender");
+// if(gender === "male"){
+//     alert("Good morning sir");
+// } 
+// if (gender === "female"){
+//     alert ("Good morning madam");
+// }
+// var trafficColor = prompt("Enter the traffic color");
+// if(trafficColor === "red"){
+//     alert("Must stop");
+// }if(trafficColor === "yellow"){
+//     alert("Ready to move");
+// }if(trafficColor === "green"){
+//     alert("Move now");
+// }
+// var fuel = +prompt("Enter your fuel quantity in litres");
+// if (fuel <= 0.25) {
+//    alert("please refill the fuel in car");
+
+// } else {
+//     alert("Your fuel is enough travelling");
+    
+// }
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+// if("car" < "cat"){
+//     alert("cat is smaller than car");
+// }
+// var totalMarks = +prompt("Enter your total marks");
+// var obtainedMarks = +prompt("Enter your obtained marks");
+// var percentage = (obtainedMarks/totalMarks)*100;
+// if(obtainedMarks < totalMarks){
+//     document.write("<h1>" + "MARK SHEET" + "</h1>");
+//     document.write("<h3>" + "TOTAL MARKS : " + totalMarks  + "</h3>");
+//     document.write("<h3>" + "OBTAINED MARKS : " + obtainedMarks + "</h3>");
+//     document.write("<h3>" + "PERCENTAGE : " + percentage + "</h3>");
+//     if(percentage >= 80 ){
+//         document.write("<h3> GRADE : A1 </h3>");
+//     }if(percentage >= 70 && percentage < 80){
+//         document.write("<h3> GRADE : A </h3>");
+//     }if(percentage >= 60 && percentage < 70){
+//         document.write("<h3> GRADE : B </h3>");
+//     }if(percentage < 60){
+//         document.write("<h3> GRADE : FAILED </h3>");
+//     }if(percentage >= 80){
+//         document.write("<h3> Remarks : Very Good </h3>");
+//     }if(percentage >= 70 && percentage < 80){
+//         document.write("<h3> Remarks : Better </h3>");
+//     }if(percentage >= 60 && percentage < 70){
+//         document.write("<h3> Remarks : Improve your work </h3>");
+//     }if(percentage < 60){
+//         document.write("<h3> Remarks : you need hard work </h3>");
+//     }
+// }else{
+//     document.write("<h1>" + "MARK SHEET" + "</h1>");
+//     document.write("<h1> YOUR INFORMATION IS INVALID </h1>");
+// }
+// var num = 7;
+// var guessNumber = +prompt("Guess the number");
+// if(guessNumber === num){
+//     document.write("<h1> “Bingo! Correct answer”");
+// }if (guessNumber === ++num) {
+//     document.write("<h1> “Close enough to the correct answer”. </h1>")    
+// }if(guessNumber !== num && guessNumber !== ++num){
+//     document.write("<h1> Thanks for trying </h1>")
+// }
+// var a = 3;
+// var b = +prompt("Enter your number");
+// if (b % a == 0) {
+//     document.write("<h1>" + b + " is divisible by 3 </h1>");
+// } else {
+//     document.write("<h1>" + b + " is not divisible by 3 </h1>");
+// }
+// var num;
+// num = +prompt("Enter your number");
+// if (num % 2 == 0) {
+//     document.write("<h1>" + num + " is a even number </h1>");
+// } else {
+//     document.write("<h1>" + num + " is a odd number </h1>");
+// }
+// var temp = +prompt("Enter temperature in celcius");
+// if (temp > 40) {
+//     document.write("<h1>“It is too hot outside.”</h1>")
+// }if (temp > 30 && temp < 40) {
+//     document.write("<h1>“The Weather today is Normal.”</h1>")
+// }if (temp > 20 && temp < 30) {
+//     document.write("<h1>“Today's Weather is cool.”</h1>")
+// }if (temp > 10 && temp < 20) {
+//     document.write("<h1>“OMG! Today's weather is so Cool.”</h1>")
+// }
